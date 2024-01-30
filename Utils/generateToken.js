34f8692bed8abdd6b32b7d47dbf653e093ad6b9a
Omar <omarsefo7@gmail.com>
2024-01-30 10:13:28 +0300

    new update

--- Utils/generateToken.js
+++ Utils/generateToken.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const generateToken = (res, userId) => {
-    const token = jwt.sign({userId}, process.env.TOKEN_KEY, {
+    const token = jwt.sign({ userId }, process.env.TOKEN_KEY, {
         expiresIn: '30d'
     });
 
@@ -11,8 +11,10 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         secure: true,
         sameSite: 'strict',
-        MaxAge: 30 * 24 * 60 * 60 * 1000,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
     });
+
+    res.status(200).json({ token });
 };
 
 export default generateToken;
