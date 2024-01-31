6987ed18ad280b063707d5267293b215e4409f95
Omar <omarsefo7@gmail.com>
2024-01-31 23:40:06 +0300

    new update

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import connection from '../db/database.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import homeRouter from '../pages/home.js';
+import adminRouter from '../pages/admin.js';
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 3001;
 
+// app.use('/api/uploads', express.static('./uploads'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
@@ -21,19 +18,8 @@ app.use(cors({
   methods: ["POST","GET","PUT","DELETE"]
 }));
 
-import homeRouter from '../pages/home.js';
-import adminRouter from '../pages/admin.js';
-
 app.use('/api', homeRouter(connection));
 app.use('/api', adminRouter(connection));
-
-// Serve static files from the 'dist' directory (Vite build output)
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route to serve 'index.html' for any other route
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
