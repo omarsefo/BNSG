2998daa868381e24f58d3d076ec8c3ee54487f44
Omar <omarsefo7@gmail.com>
2024-02-01 16:46:36 +0300

    new update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-import path, { dirname } from 'path';
+import path from 'path';
 import connection from '../db/database.js';
 import homeRouter from '../pages/home.js';
 import adminRouter from '../pages/admin.js';
@@ -11,16 +11,17 @@ app.use(cookieParser());
 app.use(express.json());
 dotenv.config();
 const port = process.env.PORT || 3001;
+
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/api/home', homeRouter(connection));
 app.use('/api/user', adminRouter(connection));
+app.use('/api/home', homeRouter(connection));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+ res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+ console.log(`Server is running on http://localhost:${port}`);
 });
