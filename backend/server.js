6931d14b9f9d02ed473538aa8e34be264cb45b63
Omar <omarsefo7@gmail.com>
2024-02-01 11:20:23 +0300

    new update

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,17 @@
 import express from 'express';
-// import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import path from 'path';
 import connection from '../db/database.js';
 import homeRouter from '../pages/home.js';
 import adminRouter from '../pages/admin.js';
+
 const app = express();
-dotenv.config();
-const port = process.env.PORT || 3001;
-
-const __dirname = path.resolve();
-
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors({
-//   origin: process.env.FRONT_URL,
-//   credentials: true,
-//   methods: ["POST","GET","PUT","DELETE"]
-// }));
+dotenv.config();
+const port = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 app.use('/api/home', homeRouter(connection));
 app.use('/api/user', adminRouter(connection));
@@ -26,7 +19,7 @@ app.use('/api/user', adminRouter(connection));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
